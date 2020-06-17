998a7f47fe09a1404e954be73dc14531555024c0
AwradArrakAlanteri-77 <aw.ala@yahoo.com>
2020-06-17 22:25:17 +0300

    Class work two is done.👍🏻

---
+++ IOS-CW2-p1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let family = ["Nasser", "Eman", "Mohammed", "Nour", "Awrad"]
+var evens = [2,4,6,8,10,12]
+var grades = [97,98,98]
+var truefalse = [true,true,true,false]
+
+print(family[0] , family[4])
+print(evens[0] , evens[5])
+print(grades[0] , grades[2])
+print(truefalse[0] , truefalse[3])
+
